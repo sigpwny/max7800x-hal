42ed86d712aee03e7a85ab3b23ee5b7c66570c8a
henopied <henopied@users.noreply.github.com>
2025-02-05 03:10:25 -0600

    chore: bump rand_core

--- src/trng.rs
+++ src/trng.rs
@@ -2,9 +2,9 @@
 //!
 //! The TRNG is a hardware module that generates random numbers using
 //! physical entropy sources.
-use rand::Error;
+use rand_core::CryptoRng;
 #[cfg(feature = "rand")]
-use rand::RngCore;
+use rand_core::RngCore;
 #[cfg(feature = "rand")]
 use rand_core::impls::{fill_bytes_via_next, next_u64_via_u32};
 
@@ -76,10 +76,7 @@ impl RngCore for Trng {
     fn fill_bytes(&mut self, dest: &mut [u8]) {
         fill_bytes_via_next(self, dest);
     }
-
-    #[inline(always)]
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
-        self.fill_bytes(dest);
-        Ok(())
-    }
 }
+
+#[cfg(feature = "rand")]
+impl CryptoRng for Trng {}
